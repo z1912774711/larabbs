2e6662b936ead0e5f64c3010a7c5b277bb3bddb5
“zhou” <1912774711@qq.com>
2019-09-20 12:44:37 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Summer</a> 设计和编码
+        </p>
+        <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p> </div>
+</footer>
